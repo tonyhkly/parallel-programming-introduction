511f7b3bb2703c12b5209e1a943f5c7304bcea47
Tony Ly <tony.ly@rightmove.co.uk>
2021-09-15 15:24:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Parallel Programming Introduction
+
+Project to introduce Parallel Programming to people for the first time
+
+## Description
+This project is a calculator where you pass a comma separated list of numbers to the `add(inputString)` method.
+
+e.g. 
+
+**Input:** "5,10,20"
+
+**Output:** 35 
+
+## Getting Started
+
+1. Open Project in intellij idea
+2. Ensure you have set the Java version in the Project Structure otherwise you will get an error
+3. When you go into the `StringCalculatorTest.java` you should see a green play button on the left
+
+![img.png](img.png)
---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringCalculator {
+
+    public int addSimple(Integer input1, Integer input2) {
+        return input1 + input2;
+    }
+
+    public int add(String inputString) {
+        String[] inputsSeperated = inputString.split(",");
+
+        if (inputsSeperated.length > 3) {
+            throw new IllegalArgumentException("Can only add up to 3 numbers but input was: " + inputString);
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String input : inputsSeperated) {
+            numbers.add(parseIntegerOrZero(input));
+        }
+
+        int total = 0;
+
+        for (Integer number : numbers) {
+            total = total + number;
+        }
+
+        return total;
+    }
+
+    private int parseIntegerOrZero(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
